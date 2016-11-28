feeee7aeb2a24e5b1e9d6ac816ae21edef1cdecb
Matija <Matija@DESKTOP-P9FTGSC>
2016-11-28 11:56:34 +0100

    Fixed issues with logging. Removed commons-logging and added slf4j with logback. Added resources folder u aplikaciji i dodan u classpath tako da Logback može pristupiti xml datoteci.

--- src/com/matija/config/AppConfig.java
+++ src/com/matija/config/AppConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -12,7 +14,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @ComponentScan({ "com.matija.*" })
 @Import({ SecurityConfig.class })
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter{
 
 	
 	@Bean
@@ -25,5 +27,9 @@ public class AppConfig {
 		return viewResolver;
 	}
 
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 	
 }
