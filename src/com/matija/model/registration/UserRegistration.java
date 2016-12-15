7bdf67be922de0b0d158c996196f2a343703acd1
MatijaCerovski <cerovskimatija@gmail.com>
2016-12-15 22:41:37 +0100

    Dodan model korisnika za registraciju

---
+++ src/com/matija/model/registration/UserRegistration.java
@@ -0,0 +1,54 @@
+package com.matija.model.registration;
+
+public class UserRegistration {
+	
+	private String username;
+	private String email;
+	private String password;
+	private String confirmedPassword;
+	
+	
+	
+	
+	public UserRegistration() {
+		super();
+	}
+
+	public UserRegistration(String username, String email, String password, String confirmedPassword) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.confirmedPassword = confirmedPassword;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmedPassword() {
+		return confirmedPassword;
+	}
+	public void setConfirmedPassword(String confirmedPassword) {
+		this.confirmedPassword = confirmedPassword;
+	}
+	
+	
+	
+	
+
+}
